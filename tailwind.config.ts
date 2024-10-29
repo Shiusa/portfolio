b4b9b3ccbab675bc1c4c8d673480d2b730cf66ed
Kyle <kylenguyen60@hotmail.com>
2024-10-29 22:35:33 +0100

    fix tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,14 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        blancPur: "#FFFFFF",
+        orTamise: "#E0C6AD",
+        bleuNeon: "#8de3ff",
+        bleuAcier: "#364f6b",
+        grisAnthracite: "#272a3a",
+        mauveNeon: "#bb8ae6",
+        mauveNeonClair: "#3D425B",
+        roseNeon: "#ff4a70",
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
