f007e1ba588902a1f78b34ea2986a3a078b85fb3
Kyle <kylenguyen60@hotmail.com>
2024-10-31 20:17:50 +0100

    fix hydration prism

--- src/components/PrismLoader.tsx
+++ src/components/PrismLoader.tsx
@@ -1,18 +1,22 @@
 'use client'
 import React, { useEffect } from 'react'
-import Prism from 'prismjs';
+// import Prism from 'prismjs';
+
 // import 'prismjs/themes/prism-okaidia.css'
 // import '../prism-atom-dark.css'
+
 import 'prismjs/themes/prism-tomorrow.css'
+
 //import 'prismjs/components/prism-tsx'
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 // require('prismjs/components/prism-jsx')
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 // require('prismjs/components/prism-tsx')
 //import 'prismjs/components/prism-typescript'
-import 'prismjs/components/prism-jsx'
-import 'prismjs/components/prism-tsx'
-import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace'
+
+// import 'prismjs/components/prism-jsx'
+// import 'prismjs/components/prism-tsx'
+// import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace'
 
 const PrismLoader = ({ children }:{ children:string }) => {
     // useEffect(() => {
@@ -30,14 +34,22 @@ const PrismLoader = ({ children }:{ children:string }) => {
     //     loadPrism()
     // }, [])
     useEffect(()=>{
-        Prism.plugins.NormalizeWhitespace.setDefaults({
-            'remove-trailing': true,      // Supprime les espaces en fin de ligne
-            'remove-indent': true,        // Supprime l'indentation
-            'left-trim': true,            // Supprime les espaces à gauche
-            'right-trim': true,           // Supprime les espaces à droite
-            'break-lines': 120             // Ajoute des sauts de ligne après 80 caractères
-        })
-        Prism.highlightAll();
+        const loadPrism = async () => {
+            const Prism = await import('prismjs');
+            await import('prismjs/components/prism-jsx');
+            await import('prismjs/components/prism-tsx');
+            await import('prismjs/plugins/normalize-whitespace/prism-normalize-whitespace');
+
+            Prism.plugins.NormalizeWhitespace.setDefaults({
+                'remove-trailing': true,      // Supprime les espaces en fin de ligne
+                'remove-indent': true,        // Supprime l'indentation
+                'left-trim': true,            // Supprime les espaces à gauche
+                'right-trim': true,           // Supprime les espaces à droite
+                'break-lines': 120             // Ajoute des sauts de ligne après 80 caractères
+            })
+            Prism.highlightAll();
+        }
+        loadPrism();
     }, []);
   return (
     <pre className='scrollbar-hidden' style={{
---
+++ src/types/prismjs.d.ts
@@ -0,0 +1,3 @@
+declare module 'prismjs/components/prism-jsx';
+declare module 'prismjs/components/prism-tsx';
+declare module 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace'
