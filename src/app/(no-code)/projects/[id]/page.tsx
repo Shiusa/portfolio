3a9fabcfff96cd4568adb15417c162634ccba1fa
Kyle <kylenguyen60@hotmail.com>
2024-11-02 01:40:24 +0100

    fix project slug page props type

--- src/app/(no-code)/projects/[id]/page.tsx
+++ src/app/(no-code)/projects/[id]/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 
-// type ProjectPageProps = {
-//   params: {
-//     slug: string;
-//   }
-// }
+type Params = Promise<{
+  slug: string
+}>
 
-const ProjectPage = async(props : { params: { slug:string } }) => {
+const ProjectPage = async(props : { params: Params }) => {
   const { slug } = await props.params;
   return (
     <div>Project {slug}</div>
