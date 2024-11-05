a11f619da5b3a33608c62a17274c52a2e7d97125
Kyle <kylenguyen60@hotmail.com>
2024-11-05 23:05:08 +0100

    add mdx project page

--- src/app/(no-code)/projects/[id]/page.tsx
+++ src/app/(no-code)/projects/[id]/page.tsx
@@ -1,4 +1,13 @@
 import React from 'react'
+import {promises as fs} from 'fs'
+import path from 'path'
+import { compileMDX } from 'next-mdx-remote/rsc'
+import { notFound } from 'next/navigation'
+import { projectData } from '@/data/data'
+import { ProjectType } from '@/types/types'
+import TechnoLabel from '../_component/TechnoLabel'
+// import { serialize } from 'next-mdx-remote/serialize'
+import WrappedImageWithText from '@/components/WrappedImageWithText'
 
 type Params = Promise<{
   id: string
@@ -7,9 +16,51 @@ type Params = Promise<{
 const ProjectPage = async(props : { params: Params }) => {
   
   const { id } = await props.params;
+  const project:ProjectType | undefined = projectData.find(project=> project.id.toString()===id);
+
+  if (!project) {
+    return notFound();
+  }
+
+  const projectsDir = path.join(process.cwd(), 'src/projects');
+  const files = await fs.readdir(projectsDir);
+
+  const fileName = files.find((file) => file.startsWith(`${id}-`) && file.endsWith('.mdx'));
+
+  if (!fileName) {
+    return notFound();
+  }
+
+  const filePath = path.join(projectsDir, fileName);
+  const content = await fs.readFile(filePath, 'utf-8');
+
+  // const filePath = path.join(process.cwd(), 'src/projects', `${id}.mdx`);
+  // const content = await fs.readFile(filePath, 'utf-8')
+  const data = await compileMDX<{id:number; title:string}>({
+    source: content,
+    options: {
+      parseFrontmatter: true
+    },
+    components: {
+      WrappedImageWithText
+    }
+  })
 
   return (
-    <div>Project {id}</div>
+    <div className='py-[8%] prose prose-headings:text-mauveNeon prose-headings:text-balance prose-h1:text-center prose-ul:leading-6 max-w-[70%] text-gray-100 overflow-y-scroll scrollbar-hidden flex flex-col prose-h1:text-orTamise'>
+      {/* <h1>Project {id}</h1> */}
+      <h1 className='text-center leading-10'>{project.title}</h1>
+      <div className='flex gap-1 flex-wrap pt-[8%] pb-[2%]'>
+        {project.techno? (
+          project.techno.map(techno => <TechnoLabel key={techno.id} name={techno.name}/>)
+        ):""}
+      </div>
+      {/* <h1>{data.frontmatter.title}</h1> */}
+      {/* <MDXRemote source={content} components={{TestWrapper}} /> */}
+      <div>
+        {data.content}
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/WrappedImageWithText.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import React from 'react'
+
+type WrappedImageWithTextType = {
+    src: string;
+    alt?: string;
+    position?: "left" | "right";
+    width?: string;
+    children: React.ReactNode;
+}
+
+const WrappedImageWithText = ({ src, alt, position, width, children }: WrappedImageWithTextType) => {
+    const direction = position === 'left' ? 'flex-row-reverse' : 'flex-row';
+
+    return (
+      <div className={`flex gap-[4%] ${direction} items-center`}>
+        {/* <div className='flex flex-col'>
+        </div> */}
+        {children}
+        <div className={`flex w-[${width}%]`}>
+          <Image
+            src={src}
+            alt={alt? alt : ""}
+            width={1920}
+            height={1080}
+            className={`object-contain object-center rounded-md border-[2px] border-orTamise`}
+            // fill
+          />
+        </div>
+      </div>
+    );
+}
+
+export default WrappedImageWithText
